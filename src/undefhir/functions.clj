cd12d3694d5b0af3ac3fd20c734b7337bc6c05aa
Aitem <muradbei@gmail.com>
2020-01-21 22:23:03 +0300

    Small refactor

--- src/undefhir/functions.clj
+++ src/undefhir/functions.clj
@@ -1,7 +1,10 @@
 (ns undefhir.functions
-  (:require
-   [clojure.data.generators :as gen]
-   [undefhir.utils :as uu]))
+  (:require [jute.core :as jute]
+            [clojure.data.generators :as gen]
+            [simple-progress.bar :as spb]
+            [undefhir.utils :as u]))
+
+
 
 (defn rand-birthDate []
   (.format
@@ -9,13 +12,51 @@
    (gen/date #inst "1990-01-16T00:00:00.000-00:00")))
 
 (defn rand-email []
-  (str (uu/rand-str 7) "@" (uu/rand-str 4) "." (rand-nth ["ru" "com" "net" "io"])))
+  (str (u/rand-str 7) "@" (u/rand-str 4) "." (rand-nth ["ru" "com" "net" "io"])))
 
 (defn rand-phone []
-  (uu/rand-nmb 10))
+  (u/rand-nmb 10))
 
 (def root-fns
   "Preset of build-in functions"
   {:randBirthDate rand-birthDate
    :randEmail rand-email
    :randPhone rand-phone})
+
+
+(defn compile-fn [{:keys [$fn $body] :as f}]
+  (let [body (jute/compile $body)]
+    (fn [& args]
+      (body (merge root-fns (zipmap (mapv keyword $fn) args))))))
+
+(defn load-fns [fns & [cb]]
+  (reduce-kv
+   (fn [acc k v]
+     (let [acc (assoc acc (keyword k) (compile-fn v))]
+       (when cb (cb))
+       acc))
+   {}
+   fns))
+
+(defn ui-load-fns [fns]
+  (println "Load functions:")
+  (let [bar (spb/mk-progress-bar (count fns))
+        result (load-fns fns bar)]
+    (println) (println)
+    result))
+
+(defn debug
+  [{manifest :manifest
+    i :input output-format :output f :function :as opts}]
+  (if output-format
+    (let [func ((keyword f) (load-fns (:fns manifest)))
+          res (apply func i)]
+      (u/formatter res output-format))
+
+    (let [func ((keyword f) (ui-load-fns (:fns manifest)))
+          res (apply func i)]
+      (println "Debug: " f)
+      (println "Input params: ")
+      (println i "\n")
+      (println "Result: ")
+      (println res))))
--- src/undefhir/template.clj
+++ src/undefhir/template.clj
@@ -2,54 +2,5 @@
   (:require [jute.core :as jute]
             [clj-yaml.core :as yaml]
             [simple-progress.bar :as spb]
+            [undefhir.functions :as fns]
             [cheshire.core :as json]))
-
-(def root-fns
-  {:randFoo #(str "foo from rand foo")})
-
-(defn compile-fn [{:keys [$fn $body] :as f}]
-  (let [body (jute/compile $body)]
-    (fn [& args]
-      (body (merge root-fns (zipmap (mapv keyword $fn) args))))))
-
-(defn load-fns [fns & [cb]]
-  (reduce-kv
-   (fn [acc k v]
-     (let [acc (assoc acc (keyword k) (compile-fn v))]
-       (when cb (cb))
-       acc))
-   {}
-   fns))
-
-(defn formatter [e f]
-  (case f
-    "json" (json/generate-stream e *out*)
-    "yaml" (println (yaml/generate-string e))
-    (throw (Exception. (str "Unsupported output format: " f)))))
-
-(defn ui-load-fns [fns]
-  (println "Load functions:")
-  (let [bar (spb/mk-progress-bar (count fns))
-        result (load-fns fns bar)]
-    (println) (println)
-    result))
-
-(defn debug
-  [{manifest :manifest
-    i :input o :output f :function :as opts}]
-  (if o
-    (let [func (get (load-fns (:fns manifest)) (keyword f))
-          res (if i
-                (apply func i)
-                (func))]
-      (formatter res o))
-
-    (let [func (get (ui-load-fns (:fns manifest)) (keyword f))
-          res (if i
-                (apply func i)
-                (func))]
-      (println "Debug: " f)
-      (println "Input params: ")
-      (println i "\n")
-      (println "Result: ")
-      (println res))))
