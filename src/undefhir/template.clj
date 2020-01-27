02df4161f0b1d8367489d75a4f8496c9f6b44cee
VictorGus <retaow@gmail.com>
2020-01-27 17:20:12 +0300

    add

--- src/undefhir/template.clj
+++ src/undefhir/template.clj
@@ -1,6 +1,1 @@
-(ns undefhir.template
-  (:require [jute.core :as jute]
-            [clj-yaml.core :as yaml]
-            [simple-progress.bar :as spb]
-            [undefhir.function :as fns]
-            [cheshire.core :as json]))
+(ns undefhir.template)
