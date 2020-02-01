ef0a23905ece553cb632a699a5e7112a6953242a
Aitem <muradbei@gmail.com>
2020-02-01 12:51:21 +0300

    Generic file stream processing

--- src/undefhir/stream.clj
+++ src/undefhir/stream.clj
@@ -11,36 +11,52 @@
 
 (defn run-output-stream
   "Get data stream from spec, get callbackk and out-stram as interface"
-  [stream-spec cb out-stream]
+  [{:keys [ file] :as spec} cb out-stream]
   (future
     (with-open [writer (io/writer out-stream)]
       (let [write-fn (fn [e]
-                       (let [res {:foo "marat"
-                                  :d (System/currentTimeMillis)  
-                                  :n (keys e)}]
-                         (json/generate-stream res  writer)
-                         (.write writer  "\n")))]
-        (doall (map write-fn (json/parsed-seq (io/reader "test/resources/stream_test.json"))))))
+                       (json/generate-stream (cb e)  writer)
+                       (.write writer  "\n"))]
+        (doseq [e (json/parsed-seq (io/reader file))]
+          (write-fn e))))
     (.close out-stream)))
 
 (defn run-input-stream
   "Receive data"
-  [spec in-stream]
-  (let [f (FileOutputStream. "test/resources/out.test22")]
-   (deref (future
-            (loop [b (.read in-stream)]
-              (when (> b -1)
-                (.write f b)
-                (recur (.read in-stream))))))))
+  [{:keys [file] :as spec} in-stream]
+  (let [f (FileOutputStream. file)]
+    (deref
+     (future
+       (loop [b (.read in-stream)]
+         (when (> b -1)
+           (.write f b)
+           (recur (.read in-stream))))))))
+
+(defn transform1 [item]
+  {:name (get item "name")})
+
+(defn transform2 [item]
+  {:gender (get item "gender")})
+
+;; TODO:
+;; reader-stream
+;; writer-stream
 
 (comment
-
   ;; PIPE
-  (def foo (future
-             (let [out-stream (PipedOutputStream.)
-                   in-stream (PipedInputStream. out-stream)]
-               (run-output-stream {} {} out-stream)
-               (run-input-stream  {} in-stream))))
+  (future-cancel foo)
+  (do
+    (def foo1 (future
+                (let [out-stream (PipedOutputStream.)
+                      in-stream (PipedInputStream. out-stream)]
+                  (run-output-stream {:file "/tmp/patient.json"} transform1 out-stream)
+                  (run-input-stream  {:file "test/resources/out.test11"} in-stream))))
+
+    (def foo2 (future
+                (let [out-stream (PipedOutputStream.)
+                      in-stream (PipedInputStream. out-stream)]
+                  (run-output-stream {:file "/tmp/patient.json"} transform2 out-stream)
+                  (run-input-stream  {:file "test/resources/out.test22"} in-stream)))))
 
 
   (let [f (FileOutputStream. "test/resources/out.test")]
