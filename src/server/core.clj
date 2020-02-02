1ae9385b23fada894be44488283992b7960e1e83
Aitem <muradbei@gmail.com>
2020-02-02 19:19:37 +0300

    Init web server

--- src/server/core.clj
+++ src/server/core.clj
@@ -1,5 +1,8 @@
 (ns server.core
   (:require [clojure.java.io :as io]
+            [route-map.core :as rm]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [org.httpkit.server :as server]
             [clojure.string :as str])
   (:import [java.io File]))
 
@@ -19,5 +22,34 @@
                       {:isDirectory (.isDirectory file)})))
         {})))
 
+(defn workspace [req]
+  {:status 200
+   :body (file-tree "src")})
+
+(def routes
+  {"workspace"  {:GET workspace}})
+
+(defn handler [{meth :request-method uri :uri :as req}]
+  (if-let [res (rm/match [meth uri] routes)]
+    ((:match res)  (update-in req [:params] merge (:params req)))
+    {:status 404 :body {:error  "Not found"}}))
+
+(def app 
+  (-> handler
+      wrap-json-response))
+
+(defonce state (atom nil))
+
+(defn stop-server []
+  (when-not (nil? @state)
+    (@state :timeout 100)
+    (reset! state nil)))
+
+(defn start-server []
+  (reset! state (server/run-server app {:port 9090})))
+
+(defn restart-server [] (stop-server) (start-server))
+
 (comment
+  (restart-server)
   (clojure.pprint/pprint (file-tree "src")))
